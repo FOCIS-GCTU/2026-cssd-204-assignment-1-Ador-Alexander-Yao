af2caad8d6207af8e3ba84a1a9bd91327b31ba87
Alexander Ador <2425402459@live.gctu.edu.gh>
2026-05-28 11:20:52 +0000

    My first python class assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,49 @@
+# File: initials.py
+# Description: A program to display my initials
+# Assignment Number: Assignment 2
+#
+# Name: Ador Alexander Yao
+# STUDENT ID:  2425402459
+# Email: 2425402459@live.gctu.edu.gh
+# Grader: Mr  Augustus Buckman
+#
+# On my honor, Ador Alexander Yao, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+  #funtion to print my initails in smaller and larger format
 
 def main():
-  pass #code goes here
+
+
+#The format of the output shall be
+
+    print("...AAY")
+
+# Displaying my initial in a larger format
+    print(".....AAAAAAAA............AAAAAAAA.........YY.........YY.....")
+    print("....AAAAAAAAAA..........AAAAAAAAAA.........YY........YY.....")
+    print("...AA........AA........AA........AA.........YY......YY......")
+    print("...AA........AA........AA........AA..........YY....YY.......")
+    print("...AA........AA........AA........AA...........YY..YY........")
+    print("...AA........AA........AA........AA............YYYY.........")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA.............YY..........")
+    print("...AA........AA...**...AA........AA...**........YY........**")
+    print("...AA........AA...**...AA........AA...**........YY........**")
+    print("...AA........AA........AA........AA.............YY..........")
+    print("...AA........AA........AA........AA.............YY..........")
+
+main() #call of the main function
+
+#end of my program .T
 
 
 
-main()
+
+
+
+
+
+
+
+
+
